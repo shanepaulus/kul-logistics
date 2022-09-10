11606825c5a2619fa77bc9d807f7f237f55c10c8
shanep <shanepaulus@gmail.com>
2022-09-10 20:49:20 +0200

    Initial commit

---
+++ src/main/java/com/kul/logistics/Application.java
@@ -0,0 +1,17 @@
+package com.kul.logistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Shane Paulus
+ * <p>
+ * Date Created: 10-Sep-2022
+ */
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
